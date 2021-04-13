672ed7587c9fb5052774eda10039aa47929bbe1d
Michael Enion <caewok_2000@yahoo.com>
2021-04-12 17:29:29 -0700

    Add .js file for setting hooks.
    Hooks on init, setup, and ready.

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,29 @@
+import { itemPatching } from './scripts/patching.ts';
+
+/* ----------------
+ * Initialize module
+ */
 Hooks.once('init', async function() {
+  console.log("spellweaver|Initializing.");
+  
 
 });
 
-Hooks.once('ready', async function() {
+/* ----------------
+ * Setup module. 
+ * Do anything after initialization but before ready
+ */
+Hooks.once('setup', async function() {
+  itemPatching();
+});
 
+
+/* ----------------
+ * When ready
+ */
+Hooks.once('ready', async function() {
+  console.log("spellweaver|Readying.")
+
+  if(!game.modules.get('lib-wrapper')?.active && game.user.isGM)
+        ui.notifications.error("Module spellweaver requires the 'libWrapper' module. Please install and activate it.");
 });
