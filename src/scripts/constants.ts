446c6ae4cb634ca652050aae041267ab0007ff05
Michael Enion <caewok_2000@yahoo.com>
2021-04-17 13:06:50 -0700

    Add a constants file for defined variables
    Place to keep defined variables, such as MODULE_ID, SPELLWEAVER_CLASSES, etc.

---
+++ src/scripts/constants.ts
@@ -0,0 +1,7 @@
+// Constants
+// May switch some of these to settings at some point
+const MODULE_ID = 'spellweaver';
+const SPELLWEAVER_CLASSES = ["Arcane Spellweaver",
+														 "Divine Spellweaver",
+														 "Natural Spellweaver",
+														 "Spellweaver"]; 
