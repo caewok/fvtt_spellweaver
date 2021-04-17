80c9dc23f2f2c29a51e7ccf9da86b6bb8c4bc6a8
Michael Enion <caewok_2000@yahoo.com>
2021-04-17 09:52:43 -0700

    gitflow-feature-stash: identify-spellweaver-rolls
    Add checks for whether a spellweaver is rolling a spell item.

--- src/scripts/itemhandling.ts
+++ src/scripts/itemhandling.ts
@@ -4,12 +4,29 @@ export async function doItemRoll(wrapped,
                                              versatile: false, 
                                              configureDialog: true, 
                                              event }) {
+  const SPELLWEAVER_CLASSES = ["Arcane Spellweaver",
+                               "Divine Spellweaver",
+                               "Natural Spellweaver",
+                               "Spellweaver"];                                           
+                                             
+  // check if this is a spell and who is rolling it
+  if("spell" === this.type && this.options.hasProperty("actor") {
+		const has_spellweaver_class = this.actor.items.some(i => "class" === i.type && SPELLWEAVER_CLASSES.includes(i.data.name));
+	  console.log(`${MODULE_ID}|Checking for spellweaver class.`);
+		if(has_spellweaver_class) {
+			console.log(`${MODULE_ID}|Spellweaver rolled a spell.`);
+	
+			 // do spellweaver stuff
+		}       
+		
+  }
+                              
   // any item roll will be wrapped
-  console.log("spellweaver|rolling item", this);
+  console.log(`${MODULE_ID}|rolling item`, this);
   const roll = await wrapped(options);
   
   // If user canceled or other error, resulting roll may be undefined.
   // Otherwise, should be chat content for the roll
-  console.log("spellweaver|roll result", roll);
+  console.log(`${MODULE_ID}|roll result`, roll);
   return roll;                                 
 }
