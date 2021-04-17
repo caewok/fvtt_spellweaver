f8b6965d3fb9bd32fb0b92135c6cb42b78eb2a28
Michael Enion <caewok_2000@yahoo.com>
2021-04-17 13:06:50 -0700

    Add a constants file for defined variables
    Place to keep defined variables, such as MODULE_ID, SPELLWEAVER_CLASSES, etc.

--- src/scripts/itemhandling.ts
+++ src/scripts/itemhandling.ts
@@ -1,32 +1,32 @@
+import * as CONSTANTS from './scripts/constants.js';
+
 export async function doItemRoll(wrapped, 
                                  options = { showFullCard: false, 
                                              createWorkflow: true, 
                                              versatile: false, 
                                              configureDialog: true, 
                                              event }) {
-  const SPELLWEAVER_CLASSES = ["Arcane Spellweaver",
-                               "Divine Spellweaver",
-                               "Natural Spellweaver",
-                               "Spellweaver"];                                           
+                                          
                                              
   // check if this is a spell and who is rolling it
-  if("spell" === this.type && this.options.hasProperty("actor") {
-		const has_spellweaver_class = this.actor.items.some(i => "class" === i.type && SPELLWEAVER_CLASSES.includes(i.data.name));
-	  console.log(`${MODULE_ID}|Checking for spellweaver class.`);
+	if("spell" === this_item.data.type && this_item.options.hasOwnProperty("actor")) {
+		// options.actor.items is a Map
+		const has_spellweaver_class = [...this_item.options.actor.items].some(i => "class" === i.type && CONSTANTS.SPELLWEAVER_CLASSES.includes(i.data.name));
+		console.log(`${CONSTANTS.MODULE_ID}|Checking for spellweaver class.`);
 		if(has_spellweaver_class) {
-			console.log(`${MODULE_ID}|Spellweaver rolled a spell.`);
-	
+			console.log(`${CONSTANTS.MODULE_ID}|Spellweaver rolled a spell.`);
+
 			 // do spellweaver stuff
 		}       
-		
-  }
+	
+	}
                               
   // any item roll will be wrapped
-  console.log(`${MODULE_ID}|rolling item`, this);
+  console.log(`${CONSTANTS.MODULE_ID}|rolling item`, this);
   const roll = await wrapped(options);
   
   // If user canceled or other error, resulting roll may be undefined.
   // Otherwise, should be chat content for the roll
-  console.log(`${MODULE_ID}|roll result`, roll);
+  console.log(`${CONSTANTS.MODULE_ID}|roll result`, roll);
   return roll;                                 
 }
