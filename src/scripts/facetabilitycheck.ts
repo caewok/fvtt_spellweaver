362e2e47d567776c4feb4e3f63eb0ca027d9c63e
Michael Enion <caewok_2000@yahoo.com>
2021-04-24 12:36:36 -0700

    Try to add a basic handlebars reference to object.

--- src/scripts/facetabilitycheck.ts
+++ src/scripts/facetabilitycheck.ts
@@ -6,10 +6,29 @@ export class FacetAbilityCheck {
   spellFacet: string;
   numEnhancements: number;
   numModifications: number;
+  spell_level: number;
+  spell_slot: number;
   dc: number;
   totalPoints: number;
   
+  // actor stats
+  
+  constructor(spell, actor) {
+    this.actor = actor;
+    this.spell = spell;
+    
+    this.numEnhancements = 0;
+    this.numModifications = 0;
+    this.spell_level = spell.data.data.level;
+    this.spell_slot = this.spell_level;
+  }
+  
   public showFacetAbilityCheck() {
     new FacetAbilityCheckDisplay(this, {}).render(true);
+  }
+  
+  public calculateDC() {
+    // fix
+    this.dc = this.spell_level + this.numEnhancements + this.numModifications;
   }
 }
